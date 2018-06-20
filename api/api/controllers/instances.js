a5a72639b28b974c21bb902f7d94bb6a39e3ee9a
AlexanderRMcNeill <alexander.r.mcneill@gmail.com>
2018-06-21 00:41:05 +1200

    Add more attributes to instance

--- api/api/controllers/instances.js
+++ api/api/controllers/instances.js
@@ -1,15 +1,25 @@
 'use strict';
 
-const uuid = require('uuid/v1')
+let idGenerator = 0
+function getId() {
+    return idGenerator++;
+}
 
 let instances = [
     {
-        id: uuid(),
-        name: "Hi"
+        id: getId(),
+        name: "Hi",
+        memory: 300,
+        type: "t2.nano",
+        private: false,
+        tag: "Srizzling rulez"
     },
     {
-        id: uuid(),
-        name: "Gophers"
+        id: getId(),
+        name: "Gophers",
+        memory: 500,
+        type: "t2.large",
+        private: true
     }
 ]
 
@@ -23,8 +33,12 @@ module.exports = {
 
 function create_instance(req, res) {
     const newInstance = {
-        id: uuid(),
-        name: req.swagger.params.body.value.name
+        id: getId(),
+        name: req.swagger.params.body.value.name,
+        memory: req.swagger.params.body.value.memory,
+        type: req.swagger.params.body.value.type,
+        private: req.swagger.params.body.value.private,
+        tag: req.swagger.params.body.value.tag,
     };
 
     instances.push(newInstance);
@@ -55,11 +69,11 @@ function read_instance(req, res) {
 }
 
 function update_instance(req, res) {
-    let found_instances = instances.filter(instance => instance.id != req.swagger.params.id.value);
+    let found_instances = instances.filter(instance => instance.id == req.swagger.params.id.value);
 
     if(found_instances.length > 1) {
         res.status(500).json({
-            message: "Error: Expected 0 "
+            message: `Error: Expected 1 but got ${found_instances.length}`
         });
 
         return;
@@ -74,6 +88,10 @@ function update_instance(req, res) {
     }
 
     found_instances[0].name = req.swagger.params.body.value.name;
+    found_instances[0].memory = req.swagger.params.body.value.memory;
+    found_instances[0].type = req.swagger.params.body.value.tag;
+    found_instances[0].private = req.swagger.params.body.value.private;
+    found_instances[0].tag = req.swagger.params.body.value.tag;
 
     res.status(200).json(found_instances[0]);
 }
@@ -96,6 +114,22 @@ function list_instances(req, res) {
 
         if(req.swagger.params.name.value){
             result = result && req.swagger.params.name.value == instance.name;
+        }
+
+        if(req.swagger.params.memory.value){
+            result = result && req.swagger.params.memory.value == instance.memory;
+        }
+
+        if(req.swagger.params.type.value){
+            result = result && req.swagger.params.type.value == instance.type;
+        }
+
+        if(req.swagger.params.tag.value){
+            result = result && req.swagger.params.tag.value == instance.tag;
+        }
+
+        if(req.swagger.params.private.value != null){
+            result = result && req.swagger.params.private.value == instance.private;
         }
 
         return result;
